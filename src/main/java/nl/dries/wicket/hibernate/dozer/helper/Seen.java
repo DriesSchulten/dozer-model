f3402ee476995f820d0ed87c3d8493906a334811
Dries Schulten <dries.schulten@gmail.com>
2012-04-12 13:55:32 +0200

    Faster/simplified endless recursion checking.

---
+++ src/main/java/nl/dries/wicket/hibernate/dozer/helper/Seen.java
@@ -0,0 +1,55 @@
+package nl.dries.wicket.hibernate.dozer.helper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Seen objects holder
+ * 
+ * @author dries
+ */
+public class Seen
+{
+	/** */
+	List<Object> seen = new ArrayList<>();
+
+	/**
+	 * @param obj
+	 *            the object to mark as seen
+	 */
+	public void add(Object obj)
+	{
+		seen.add(obj);
+	}
+
+	/**
+	 * @param obj
+	 *            the object to remove
+	 */
+	public void remove(Object obj)
+	{
+		seen.remove(obj);
+	}
+
+	/**
+	 * Checks if the seen list contains a given object
+	 * 
+	 * @param obj
+	 *            the object
+	 * @return <code>true</code>
+	 */
+	public boolean contains(Object obj)
+	{
+		boolean contains = false;
+
+		for (Object seenObj : seen)
+		{
+			if (!contains)
+			{
+				contains = seenObj == obj;
+			}
+		}
+
+		return contains;
+	}
+}
--- src/main/java/nl/dries/wicket/hibernate/dozer/visitor/HibernateObjectVisitor.java
+++ src/main/java/nl/dries/wicket/hibernate/dozer/visitor/HibernateObjectVisitor.java
@@ -50,23 +50,18 @@ public class HibernateObjectVisitor implements VisitorStrategy
 	/** */
 	private final ClassMetadata metadata;
 
-	/** */
-	private final Object previous;
-
 	/**
 	 * Construct
 	 * 
 	 * @param sessionImpl
+	 * @param callback
 	 * @param metadata
-	 * @param previous
 	 */
-	public HibernateObjectVisitor(SessionImplementor sessionImpl, ModelCallback callback, ClassMetadata metadata,
-		Object previous)
+	public HibernateObjectVisitor(SessionImplementor sessionImpl, ModelCallback callback, ClassMetadata metadata)
 	{
 		this.sessionImpl = sessionImpl;
 		this.callback = callback;
 		this.metadata = metadata;
-		this.previous = previous;
 	}
 
 	/**
@@ -86,7 +81,7 @@ public class HibernateObjectVisitor implements VisitorStrategy
 			{
 				Object value = ObjectHelper.getValue(object, propertyName);
 
-				if (value != null && !value.equals(previous))
+				if (value != null)
 				{
 					Object[] logVals = new Object[] { identifier, metadata.getMappedClass().getName(), propertyName };
 
@@ -145,7 +140,8 @@ public class HibernateObjectVisitor implements VisitorStrategy
 			List list = (List) plainCollection;
 			for (Iterator<?> iter = ((PersistentBag) collection).iterator(); iter.hasNext();)
 			{
-				list.add(ObjectHelper.deproxy(iter.next()));
+				Object next = iter.next();
+				list.add(ObjectHelper.deproxy(next));
 			}
 		}
 		else if (plainCollection instanceof Set<?>)
@@ -153,7 +149,8 @@ public class HibernateObjectVisitor implements VisitorStrategy
 			Set set = (Set) plainCollection;
 			for (Iterator<?> iter = ((PersistentSet) collection).iterator(); iter.hasNext();)
 			{
-				set.add(ObjectHelper.deproxy(iter.next()));
+				Object next = iter.next();
+				set.add(ObjectHelper.deproxy(next));
 			}
 		}
 		else
